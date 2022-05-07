627c44b5446d9fa3b6fdb7bb2d95d984e3b0e767
Andrew Babbitt <andrew.babbitt@outlook.com>
2022-05-07 15:21:00 -0600

    Try again

--- src/Docs/Program.cs
+++ src/Docs/Program.cs
@@ -1,7 +1,13 @@
 namespace Docs;
 
+/// <summary>
+/// Test
+/// </summary>
 public class Program
 {
+    /// <summary>
+    /// Test
+    /// </summary>
     public static void Main()
     {
         Console.WriteLine("Hello World");
